a43bf290c97b6c06977adc48774cc2137f95677e
bpdiego0907 <diegoblpach09@gmail.com>
2026-03-03 11:06:14 -0500

    logo y flyer corregidos

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           }}
         >
           <img
-            src="/logo.jpeg"
+            src="/logo.png"
             alt="Logo Club Power"
             style={{
               width: 420,
